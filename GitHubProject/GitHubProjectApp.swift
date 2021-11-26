4f26195b5982d17e33d8de7f9a6926d8370fff68
miravicson <victorughonu95@gmail.com>
2021-11-26 20:05:47 +0100

    Initial Commit

---
+++ GitHubProject/GitHubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProjectApp.swift
+//  GitHubProject
+//
+//  Created by Victor Ughonu on 26/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
